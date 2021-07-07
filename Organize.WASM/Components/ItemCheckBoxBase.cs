175a25999110762046b7f40132c3c74f9db6f806
ROBERTO PAULA <roberto.paula01@fatec.sp.gov.br>
2021-07-07 13:49:46 -0300

    End of Section 7

---
+++ Organize.WASM/Components/ItemCheckBoxBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Organize.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organize.WASM.Components
+{
+    public class ItemCheckBoxBase : ComponentBase
+    {
+        [Parameter]
+        public BaseItem Item { get; set; }
+
+        public ItemCheckBoxBase()
+        {
+            Item = new BaseItem();
+        }
+
+        [CascadingParameter]
+        public string ColorPrefix { get; set; }
+    }
+}
--- Organize.WASM/Components/ItemElement.razor.cs
+++ Organize.WASM/Components/ItemElement.razor.cs
@@ -10,6 +10,25 @@ namespace Organize.WASM.Components
     public partial class ItemElement<TItem> : ComponentBase where TItem : BaseItem
     {
         [Parameter]
+        public RenderFragment MainFragment { get; set; }
+        
+        [Parameter]
+        public RenderFragment DetailFragment { get; set; }
+
+        [Parameter]
         public TItem Item { get; set; }
+
+        [CascadingParameter]
+        public string ColorPrefix { get; set; }
+        
+        [CascadingParameter]
+        public int TotalNumber { get; set; }
+        private string DetailAreaId { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            DetailAreaId = "detailArea" + Item.Position;
+        }
     }
 }
